f99a4b9bdbb7c08087477b35b1a5a44d3231ade5
SimonBao <Simonbao.sb@gmail.com>
2021-09-23 01:50:02 +0100

    implemented design for homepage

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import "../styles/about.scss";
+import avatar from "../images/avatar-about.png";
+import { ProgressBar } from 'react-bootstrap';
 const About = () => {
-  return <div className="page" id="about">About</div>;
+  return (
+    <div className="page" id="about">
+      <div className="col-12">
+        <h1 className="ml-2">About Me</h1>
+      </div>
+      <div className="col-12 d-flex flex-row">
+        <div className="col-4 d-flex justify-content-center">
+          <img src={avatar} alt="about-avatar" />
+        </div>
+        <div className="col-7 d-flex align-items-center about__card">
+          <div className="col-12 d-flex flex-row">
+            <div className="col-5 introduction">
+              <span>
+                ffdsdha, skjdfh kajsdhf kjsahd f j ksahdfjk sadhfkl asjh f
+                klsjad hfkas jdh flksj dha fsha dsdha, skjdfh kajsdhf kjsahd f j
+                ksahdfjk sadhfkl asjh f klsjad hfkas jdh flksj dha fsha
+              </span>
+            </div>
+            <div className="col-7">
+  <ProgressBar now="70" visuallyHidden />
+    </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,59 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import Typed from "react-typed";
+import avatar from "../images/avatar.png";
+import "../styles/homepage.scss";
 
 const Introduction = () => {
+  document.addEventListener("mousemove", parallax);
+  function parallax(e) {
+    this.querySelectorAll(".layer").forEach((layer) => {
+      const speed = layer.getAttribute("data-speed");
+
+      const x = (window.innerWidth - e.pageX * speed) / 100;
+      const y = (window.innerHeight - e.pageY * speed) / 100;
+
+      layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+  }
   return (
-    <div
-      className="page introduction d-flex flex-column justify-content-center align-items-center"
-      id="home"
-    >
-      <h1>Simon Bao</h1>
-      <Typed
-        className="mb-2"
-        strings={[
-          "Hi my name is Simon",
-          "I am a Software Engineer/Web Developer",
-          "Also an avid gamer",
-        ]}
-        typeSpeed={40}
-        backSpeed={60}
-        loop
-      />
-      <div className="d-flex flex-row">
-        <a
-          href="https://www.linkedin.com/in/simon-bao"
-          className="icon"
-          target="_blank"
-        >
-          <FaLinkedin />
-        </a>
-        <a href="https://github.com/SimonBao" className="icon" target="_blank">
-          <FaGithub />
-        </a>
+    <div className="page homepage" id="home">
+      <div
+        className="d-flex flex-column justify-content-center align-items-center layer"
+        data-speed="5"
+      >
+        <img src={avatar} className="homepage_avatar" alt="avatar" />
+        <h1>Simon Bao</h1>
+        <Typed
+          className="mb-2 typed-text pl-2"
+          strings={[
+            "Hi my name is Simon",
+            "I am a Software Engineer",
+            "Web Developer",
+            "And also an avid gamer",
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <div className="d-flex flex-row justify-content-center">
+          <a
+            href="https://www.linkedin.com/in/simon-bao"
+            rel="noreferrer"
+            className="icon"
+            target="_blank"
+          >
+            <FaLinkedin size={35} />
+          </a>
+          <a
+            href="https://github.com/SimonBao"
+            className="icon"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaGithub size={35} />
+          </a>
+        </div>
       </div>
     </div>
   );
